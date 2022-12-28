e9544d14484a30fc3364ce8e3184b961b7a23e1d
moolleer <m_moller@live.se>
2022-12-28 18:26:20 +0000

    Add functions for showing and hiding game instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 
 
+let gameInstructions = document.getElementById('instructions');
 
+
+function showGameInstructions() {
+    gameInstructions.classList.remove('hide');
+}
+
+function hideGameInstructions(){
+    gameInstructions.classList.add('hide');
+}
+
+let howToPlay = document.getElementById('how-to-play');
+howToPlay.addEventListener('click', showGameInstructions);
+
+let hideHowToPlay = document.getElementById('close');
+hideHowToPlay.addEventListener('click', hideGameInstructions);
