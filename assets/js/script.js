b31ae2814f7ec42fb10784a434c92f8266b657d5
moolleer <m_moller@live.se>
2022-12-29 11:26:01 +0000

    Add feedback form and functions for show/hide form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 
 let gameInstructions = document.getElementById('instructions');
+let feedbackForm = document.getElementById('feedback');
 
 
 function showGameInstructions() {
@@ -11,8 +12,24 @@ function hideGameInstructions(){
     gameInstructions.classList.add('hide');
 }
 
+function showFeedbackForm() {
+    feedbackForm.classList.remove('hide');
+}
+
+function hideFeedbackForm() {
+    feedbackForm.classList.add('hide');
+}
+
+// Eventlisteners
+
 let howToPlay = document.getElementById('how-to-play');
 howToPlay.addEventListener('click', showGameInstructions);
 
 let hideHowToPlay = document.getElementById('close');
 hideHowToPlay.addEventListener('click', hideGameInstructions);
+
+let feedbackButton = document.getElementById('feedback-button');
+feedbackButton.addEventListener('click', showFeedbackForm);
+
+let closeForm = document.getElementById('close-form');
+closeForm.addEventListener('click', hideFeedbackForm);
