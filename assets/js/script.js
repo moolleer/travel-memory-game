4e3895eafb1d911cc39d329798b5c3cf9c5deacc
moolleer <m_moller@live.se>
2022-12-29 19:04:59 +0000

    Add array of image object and function for showing game modual

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
+//create an array of travel images
+const itemsArray = [
+    {name: 'plane', image: 'plane.png'},
+    {name: 'camera', image: 'camera.png'},
+    {name: 'drink', image: 'drink.png'},
+    {name: 'flip-flop', image: 'flip-flop.png'},
+    {name: 'map', image: 'map.png'},
+    {name: 'passport', image: 'passport.png'},
+    {name: 'photos', image: 'photos.png'},
+    {name: 'suitcase', image: 'suitcase.png'},
+];
+
 
 
 let gameInstructions = document.getElementById('instructions');
 let feedbackForm = document.getElementById('feedback');
+let showGameGrid = document.getElementById('game-modual');
 
 
 function showGameInstructions() {
@@ -20,6 +33,11 @@ function hideFeedbackForm() {
     feedbackForm.classList.add('hide');
 }
 
+function showGame() {
+    showGameGrid.classList.remove('hide');
+    
+}
+
 // Eventlisteners
 
 let howToPlay = document.getElementById('how-to-play');
@@ -33,3 +51,6 @@ feedbackButton.addEventListener('click', showFeedbackForm);
 
 let closeForm = document.getElementById('close-form');
 closeForm.addEventListener('click', hideFeedbackForm);
+
+let startGameButton = document.getElementById('new-game');
+startGameButton.addEventListener('click', showGame);
